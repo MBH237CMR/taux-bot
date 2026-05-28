111475a490463e29b2a2ebf1f584023debb1af35
MBH237CMR <ousman.imbh237@gmail.com>
2026-05-28 23:05:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,33 @@
 import os
 import requests
-from telegram import Bot
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def get_rate():
-    url = "https://api.exchangerate-api.com/v4/latest/CAD"
-    r = requests.get(url).json()
-    rate = r["rates"]["XAF"]
-    return rate
+def get_tx():
+    r = requests.get("https://api.exchangerate.host/latest?base=CAD&symbols=XAF", timeout=10)
+    r.raise_for_status()
+    rate_google = float(r.json()["rates"]["XAF"])
+    tx = rate_google * 1.0055
+    return tx
 
-def send_message():
-    rate = get_rate()
-    text = f"💱 Taux CAD → XAF\n1 CAD = {rate:.2f} XAF"
-    bot = Bot(token=BOT_TOKEN)
-    bot.send_message(chat_id=CHANNEL_ID, text=text)
+def main():
+    tx = get_tx()
+    msg = f"""🇨🇦🇨🇲Taux de change Live🇨🇦🇨🇲
+           💰 ♻️💲
+
+_*1 CAD = {tx:.2f} CFA*_
+
+_*100 CAD = {tx*100:.0f} CFA*_
+
+_*500 CAD = {tx*500:.0f} CFA*_
+
+          💱 💰 💱 _XAF_"""
+    
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        json={"chat_id": TELEGRAM_CHAT_ID, "text": msg, "parse_mode": "Markdown"}
+    )
 
 if __name__ == "__main__":
-    send_message()
+    main()
